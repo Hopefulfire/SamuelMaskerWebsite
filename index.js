8f6dc6da1a692e01582103931d60f3aa2e24c102
Hopefulfire <Hopefulfireus@gmail.com>
2025-02-07 00:59:11 -0500

    made the animation fluid this took way too long TwT

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ let clickable = 0;
 function OpenNav(){
     if(clickable == 0){
         document.getElementById("nav").classList = "nav-animation-open"
+        document.getElementById("content").style.flex = "90%"
         clickable = 1;
     }else if(clickable == 1){
         document.getElementById("nav").classList = "nav-animation-close"
+        document.getElementById("content").style.flex = "100%"
         clickable = 0;
     }else{
         location.reload
